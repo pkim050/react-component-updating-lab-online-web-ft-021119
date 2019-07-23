970f94d0eec4f3dd1cdf570c6b2be7bfdb9969ab
Patrick Kim <pkim050@gmail.com>
2019-07-23 20:01:29 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -27,6 +27,13 @@ class Timer extends Component {
     this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
   render() {
     const { time, color, logText } = this.state;
     return (
