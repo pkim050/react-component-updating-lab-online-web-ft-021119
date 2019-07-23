57c57b978a3e3c6a7ae0384e605daa06e051e3aa
Patrick Kim <pkim050@gmail.com>
2019-07-23 19:34:03 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -23,6 +23,10 @@ class Timer extends Component {
     clearInterval(this.interval);
   }
 
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
   render() {
     const { time, color, logText } = this.state;
     return (
